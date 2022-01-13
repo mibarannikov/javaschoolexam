0b38873bcf7bc1a6ae48e463102e83a94541ba8e
Admin <Admin>
2022-01-13 10:23:16 +0300

    pyramid and subsequence

--- src/main/java/com/tsystems/javaschool/tasks/pyramid/PyramidBuilder.java
+++ src/main/java/com/tsystems/javaschool/tasks/pyramid/PyramidBuilder.java
@@ -1,8 +1,15 @@
 package com.tsystems.javaschool.tasks.pyramid;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 
 public class PyramidBuilder {
+    public static void main(String[] args) {
+        //new PyramidBuilder().buildPyramid(new ArrayList<Integer>(Arrays.asList(4, 1, 2, 5, 6, 3, 7)));
+
+  }
 
     /**
      * Builds a pyramid with sorted values (with minumum value at the top line and maximum at the bottom,
@@ -12,9 +19,25 @@ public class PyramidBuilder {
      * @return 2d array with pyramid inside
      * @throws {@link CannotBuildPyramidException} if the pyramid cannot be build with given input
      */
-    public int[][] buildPyramid(List<Integer> inputNumbers) {
-        // TODO : Implement your solution here
-        return new int[0][0];
+    public int[][] buildPyramid(List<Integer> inputNumbers)  {
+        double pyramidHeight = 0.5 + Math.sqrt(1 + 8 * inputNumbers.size()) / 2 - 1;
+        if (pyramidHeight - Math.floor(pyramidHeight) != 0) throw new CannotBuildPyramidException();
+        try {
+            inputNumbers.sort(Comparator.naturalOrder());
+        } catch (NullPointerException e) {
+            throw new CannotBuildPyramidException();
+        }
+
+        int[][] pyramid = new int[(int) pyramidHeight][(int) (2 * pyramidHeight - 1)];
+        int indexColumnTopOfThePyramid = (int) pyramidHeight - 1;
+        int countInputNambers = 0;
+        for (int i = 0; i < pyramidHeight; i++) {
+            for (int j = indexColumnTopOfThePyramid - i; j <= indexColumnTopOfThePyramid + i; j = j + 2) {
+                pyramid[i][j] = inputNumbers.get(countInputNambers);
+                countInputNambers++;
+            }
+        }
+        return pyramid;
     }
 
 
--- src/main/java/com/tsystems/javaschool/tasks/subsequence/Subsequence.java
+++ src/main/java/com/tsystems/javaschool/tasks/subsequence/Subsequence.java
@@ -3,6 +3,9 @@ package com.tsystems.javaschool.tasks.subsequence;
 import java.util.List;
 
 public class Subsequence {
+    public static void main(String[] args) {
+
+    }
 
     /**
      * Checks if it is possible to get a sequence which is equal to the first
@@ -13,8 +16,25 @@ public class Subsequence {
      * @return <code>true</code> if possible, otherwise <code>false</code>
      */
     @SuppressWarnings("rawtypes")
-    public boolean find(List x, List y) {
-        // TODO: Implement the logic here
+    public boolean find(List x, List y) throws IllegalArgumentException {
+        if (x == null | y == null) throw new IllegalArgumentException();
+        if (x.size() == 0) {
+            return true;
+        }
+        if (x.size() == 0 & y.size() == 0) {
+            return true;
+        }
+        int i = 0;
+        for (int j = 0; j < y.size(); j++) {
+            if (x.get(i).equals(y.get(j))) {
+                i++;
+            }
+            if (i == x.size() - 1) {
+                return true;
+            }
+
+        }
+
         return false;
     }
 }
